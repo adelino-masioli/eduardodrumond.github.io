37af1d977a975606824a37dda569e35222f63a91
alfjuniorbh <alfjuniorbh.web@gmail.com>
2017-06-14 08:24:35 -0300

    First

---
+++ index.php
@@ -0,0 +1,233 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Eduardo Drumond</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Eduardo Drumond" />
+    <meta name="keywords" content="Eduardo Drumond atua no mercado imobiliário desde 2002. Sua dedicação, conhecimento e experiência o tornaram um corretor de imóveis altamente qualificado e pronto para fazer bons negócios na compra e venda de imóveis." />
+    <meta name="author" content="juniorferreira.com.br" />
+
+
+    <!-- Facebook and Twitter integration -->
+    <meta property="og:title" content=""/>
+    <meta property="og:image" content=""/>
+    <meta property="og:url" content=""/>
+    <meta property="og:site_name" content=""/>
+    <meta property="og:description" content=""/>
+    <meta name="twitter:title" content="" />
+    <meta name="twitter:image" content="" />
+    <meta name="twitter:url" content="" />
+    <meta name="twitter:card" content="" />
+    <link href="images/favicon.png" rel="shortcut icon" type="image/vnd.microsoft.icon" />
+
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:300,400" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:300,400,500" rel="stylesheet">
+
+    <!-- Animate.css -->
+    <link rel="stylesheet" href="css/animate.css">
+    <!-- Icomoon Icon Fonts-->
+    <link rel="stylesheet" href="css/icomoon.css">
+    <!-- Bootstrap  -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+
+    <!-- Magnific Popup -->
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <!-- Owl Carousel  -->
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+    <!-- Flexslider  -->
+    <link rel="stylesheet" href="css/flexslider.css">
+
+    <!-- Pricing -->
+    <link rel="stylesheet" href="css/pricing.css">
+
+    <!-- Theme style  -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- Modernizr JS -->
+    <script src="js/modernizr-2.6.2.min.js"></script>
+    <!-- FOR IE9 below -->
+    <!--[if lt IE 9]>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+
+<div class="fh5co-loader"></div>
+
+<div id="page">
+    <nav class="fh5co-nav" role="navigation">
+        <div class="top-menu">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-2">
+                        <div id="fh5co-logo"><a href="/"><img src="images/brand.png" alt=""></a></div>
+                    </div>
+                    <div class="col-xs-10 text-right menu-1">
+                        <ul>
+                            <li class="active"><a href="/">Home</a></li>
+                            <li><a href="venda-de-imoveis.html">Venda de imóveis</a></li>
+                            <li><a href="compra-de-imoveis.html">Compra de imóveis</a></li>
+                            <li><a href="parceria-com-imobiliaria.html">Parceria com imobiliárias</a></li>
+                            <li><a href="eduardo-drumond.html">Eduardo Drumond</a></li>
+                            <li><a href="contato.html">Contato</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </nav>
+
+    <aside id="fh5co-hero">
+        <div class="flexslider">
+            <ul class="slides">
+                <li style="background-image: url(images/img_bg_1.jpg);">
+                    <div class="overlay-gradient"></div>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12 text-center slider-text">
+                                <div class="slider-text-inner">
+                                    <h1>O atendimento é tão exclusivo que até a sua confiança vai ser única.</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </aside>
+
+    <div id="fh5co-course-categories">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-4 col-sm-4 col-xs-12 text-center animate-box">
+                    <div class="services">
+                        <div class="desc">
+                            <h3>Venda de imóveis</h3>
+                            <p class="blocotexto">Pense numa empresa que cuida de cada detalhe
+                                da venda do seu imóvel, até da sua tranquilidade.
+                                A Eduardo Drumond Assessoria Imobiliária
+                                acompanha você em todo o processo de venda:
+                                da avaliação do imóvel à assinatura do contrato.
+                            </p>
+                            <p><a href="#" class="btn-saiba-mais">Saiba mais</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-4 col-xs-12 text-center animate-box b2">
+                    <div class="services">
+                        <div class="desc">
+                            <h3>Compra de imóveis</h3>
+                            <p class="blocotexto">A Eduardo Drumond Assessoria Imobiliária acompanha você em cada passo do processo de compra: da primeira visita ao fechamento do contrato. Nossa dedicação é tão grande, que você vai se sentir realizado antes de alcançar seu sonho.</p>
+                            <p><a href="#" class="btn-saiba-mais">Saiba mais</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-4 col-xs-12 text-center animate-box b3">
+                    <div class="services">
+                        <div class="desc">
+                            <h3>Parceria com imobiliárias</h3>
+                            <p class="blocotexto">Imagine ter uma parceira que compartilha tudo com a sua imobiliária, da satisfação do cliente à comissão. A Eduardo Drumond Assessoria Imobiliária é assim. Acompanhamos os clientes em todo o processo de venda: da avaliação do imóvel (clientes vendedores) ou primeira visita (clientes compradores) à assinatura dos contratos.
+                            <p><a href="#" class="btn-saiba-mais">Saiba mais</a></p>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="fh5co-testimonial"></div>
+
+    <div id="fh5co-blog">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6 col-sm-6 col-xs-12  animate-box eduardofoto"></div>
+                <div class="col-md-6 col-sm-6 col-xs-12 animate-box eduardonome">
+                    <h3><img src="images/eduardo_nome.png" alt="Eduardo Drumond"></h3>
+                    <p>Eduardo Drumond atua no mercado imobiliário desde 2002. Sua dedicação, conhecimento e experiência o tornaram um corretor de imóveis altamente qualificado e pronto para fazer bons negócios na compra e venda de imóveis.</p>
+                    <p><a href="#" class="btn-saiba-mais">Saiba mais</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <footer id="fh5co-footer" role="contentinfo" style="background-image: url(images/img_bg_4.jpg);">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row row-pb-md">
+                <div class="col-md-3 col-sm-4 col-xs-6 col-md-push-1 fh5co-widget">
+                    <h3>Compra e venda</h3>
+                    <ul class="fh5co-footer-links">
+                        <li><a href="venda-de-imoveis.html">Venda de imóveis</a></li>
+                        <li><a href="compra-de-imoveis.html">Compra de imóveis</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-md-3 col-sm-4 col-xs-6 col-md-push-1 fh5co-widget">
+                    <h3>Parcerias</h3>
+                    <ul class="fh5co-footer-links">
+                        <li><a href="parceria-com-imobiliaria.html">Parceria com imobiliárias</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-md-3 col-sm-4 col-xs-6 col-md-push-1 fh5co-widget">
+                    <h3>Sobre</h3>
+                    <ul class="fh5co-footer-links">
+                        <li><a href="eduardo-drumond.html">Eduardo Drumond</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-md-3 col-sm-4 col-xs-6 col-md-push-1 fh5co-widget">
+                    <h3>Contato</h3>
+                    <ul class="fh5co-footer-links">
+                        <li><a href="contato.html">Fale conosco</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="row copyright">
+                <div class="col-md-12 text-center">
+                    <p>
+                        <small class="block">&copy; 2017 Eduardo Drumond. Todos os direitos reservados.</small>
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    </footer>
+</div>
+
+<div class="gototop js-top">
+    <a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+</div>
+
+<!-- jQuery -->
+<script src="js/jquery.min.js"></script>
+<!-- jQuery Easing -->
+<script src="js/jquery.easing.1.3.js"></script>
+<!-- Bootstrap -->
+<script src="js/bootstrap.min.js"></script>
+<!-- Waypoints -->
+<script src="js/jquery.waypoints.min.js"></script>
+<!-- Stellar Parallax -->
+<script src="js/jquery.stellar.min.js"></script>
+<!-- Carousel -->
+<script src="js/owl.carousel.min.js"></script>
+<!-- Flexslider -->
+<script src="js/jquery.flexslider-min.js"></script>
+<!-- countTo -->
+<script src="js/jquery.countTo.js"></script>
+<!-- Main -->
+<script src="js/main.js"></script>
+</body>
+</html>
